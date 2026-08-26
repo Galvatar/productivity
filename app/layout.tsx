95080f2bddd9b97d2930eb08eda3c6150f621651
Michael Boyer <mboy221@aucklanduni.ac.nz>
2026-08-26 22:00:26 +1200

    Added vercel analytics component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 import Header from "./components/header";
 
@@ -24,6 +25,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <Analytics />
       <body className="h-screen flex flex-col">
         <Header />
         {children}
